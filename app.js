1a766aaa6cefd03da10d225dbea0c3b89405ab9e
CyberMedina <jhonatanmedina5255@gmail.com>
2025-01-06 01:00:22 -0600

    primer commit

---
+++ app.js
@@ -0,0 +1,119 @@
+const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
+
+const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
+
+const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
+    [
+        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
+        'https://bot-whatsapp.netlify.app/',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
+    [
+        '🙌 Aquí encontras un ejemplo rapido',
+        'https://bot-whatsapp.netlify.app/docs/example/',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
+    [
+        '🚀 Puedes aportar tu granito de arena a este proyecto',
+        '[*opencollective*] https://opencollective.com/bot-whatsapp',
+        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
+        '[*patreon*] https://www.patreon.com/leifermendez',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowDiscord = addKeyword(['discord']).addAnswer(
+    ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
+    null,
+    null,
+    [flowSecundario]
+)
+
+
+
+const axios = require('axios');
+const envioPruebaAPI = async (numero) => {
+  try {
+    let res = await axios.post('http://127.0.0.1:7000/api/prueba', {
+        "numero": numero
+    });
+    // Aquí están los servicios
+    let datosCliente = res.data;
+    return datosCliente;
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+
+const flowPrueba = addKeyword(['hola', 'adios']).addAnswer("XD", null, async(ctx, ctxFn) => {
+    console.log("Mensaje recibido", ctx)
+}   )
+
+
+const flowPrincipal = addKeyword(EVENTS.MEDIA).addAnswer("te amito", null, async(ctx, ctxFn) => {
+
+
+    if (ctx?.message?.imageMessage) {
+        numero = ctx.from;
+        let resultado = await envioPruebaAPI(numero);
+    }
+
+
+    console.log("Mensaje recibido", ctx)
+
+    })
+
+
+
+
+
+// const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
+//     .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
+//     .addAnswer(
+//         [
+//             'te comparto los siguientes links de interes sobre el proyecto',
+//             '👉 *doc* para ver la documentación',
+//             '👉 *gracias*  para ver la lista de videos',
+//             '👉 *discord* unirte al discord',
+//         ],
+//         null,
+//         null,
+//         [flowDocs, flowGracias, flowTuto, flowDiscord]
+//     )
+
+const main = async () => {
+    const adapterDB = new MockAdapter()
+    const adapterFlow = createFlow([flowPrincipal], [flowPrueba])
+    const adapterProvider = createProvider(BaileysProvider)
+
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    QRPortalWeb()
+}
+
+main()
